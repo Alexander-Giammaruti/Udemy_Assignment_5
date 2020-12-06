318f8da805097ab84e291325ffb8ea0d85f8739a
Alexander Giammaruti <44341602+Alexander-Giammaruti@users.noreply.github.com>
2020-12-05 20:58:37 -0600

    converted storage and communication of 'users' from event emitters to a centralized service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit { 
+  activeUsers: string[];
+  inactiveUsers: string[];
+
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit() {
+    this.activeUsers = this.usersService.activeUsers;
+    this.inactiveUsers = this.usersService.inactiveUsers;
+  }
+
+  onSetToInactive(id: number) {
+    this.usersService.onSetToActive(id);
+  }
+
+  onSetToActive(id: number) {
+    this.usersService.onSetToInactive(id);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
+import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { UsersService } from './users.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActiveUsersComponent,
+    InactiveUsersComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
